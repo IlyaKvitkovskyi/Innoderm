14819cee87f8bca738c1b77d07f27e8068fd1075
Illia Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2022-11-15 12:48:36 +0200

    added test services page

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(function () {
   $('.menu-btn').on('click', function (event) {
     $('.menu-btn, .header__menu-content').toggleClass('active');
     $('header').toggleClass('white');
+    $('.page').toggleClass('blur');
   });
   //   //for slick slider
   //   $('.slider').slick({
