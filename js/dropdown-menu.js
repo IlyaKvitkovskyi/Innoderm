d59d190d9678e34e7e8edfa0ccddb7e774b4bd89
Illia Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2022-11-22 14:15:38 +0200

    fixed dropdown menu

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,7 +1,11 @@
-const buttons = document.querySelectorAll('.burger-menu__item');
+const buttons = document.querySelectorAll('.arrow');
+const item = document.querySelector('.burger-menu__item');
+const list = document.querySelector('.burger-submenu__list');
 
 for (const button of buttons) {
   button.addEventListener('click', () => {
     button.classList.toggle('active');
+    list.classList.toggle('active');
+    item.classList.toggle('active');
   });
 }
--- js/index.js
+++ js/index.js
@@ -10,12 +10,33 @@ $(function () {
   $('.burger').on('click', function (event) {
     $('.burger, .burger-wrap, .burger-menu').toggleClass('active');
     $('header').toggleClass('white');
+    $('html').toggleClass('lock');
   });
-}); 
+});
+
 $(function () {
-  $('.menu-btn').on('click', function (event) {
-    $('.menu-btn, .header__menu-content').toggleClass('active');
-    $('header').toggleClass('white');
-    $('.page').toggleClass('blur');
+  var header = $('#header'),
+    introH = $('#intro').innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  let burger = $('#burger');
+
+  /* Fixed header */
+  checkScroll(scrollOffset);
+
+  $(window).on('scroll', function () {
+    scrollOffset = $(this).scrollTop();
+
+    checkScroll(scrollOffset);
   });
+
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= introH) {
+      header.addClass('fixed');
+      burger.addClass('black');
+    } else {
+      header.removeClass('fixed');
+      burger.removeClass('black');
+    }
+  }
 });
